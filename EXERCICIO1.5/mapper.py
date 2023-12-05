81549b8bdbd6386f64297c1a741ac76ee8e87255
tsB7OdINBSnFD+H4my8Otw== <xuwira33@cdh61-login8.bd.cluster.cesga.es>
2023-12-05 19:43:20 +0100

    Exercicio 5

--- EXERCICIO1.5/mapper.py
+++ EXERCICIO1.5/mapper.py
@@ -10,6 +10,7 @@ import sys
 
 for line in sys.stdin:
     data = line.strip().split("\t")
+    if len(data)!=6: continue;
     date, time, store, item, cost, payment = data
-    print(store+"\t"+cost)
+    print("Total Ventas\t"+cost)
 
